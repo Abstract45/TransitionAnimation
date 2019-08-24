03a6bbea5196e1e325ed4b4f4cb136c40e8b4223
Miwand <miwand.najafe@gmail.com>
2019-08-24 11:43:59 -0400

    Initial Commit

---
+++ TransitionAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionAnimation
+//
+//  Created by Miwand Najafe on 2019-08-24.
+//  Copyright © 2019 Miwand Najafe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
